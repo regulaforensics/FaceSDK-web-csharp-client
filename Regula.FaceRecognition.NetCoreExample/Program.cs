c153aa57b67fa7d206d1ce290cc0ad3804c4732c
Danila Kislitsyn <dangost16@gmail.com>
2021-04-05 10:34:04 +0300

    init solution

---
+++ Regula.FaceRecognition.NetCoreExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Regula.FaceRecognition.NetCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

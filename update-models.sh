4712244a934bb3526457c21a48077d6dabc41c62
Hleb Albau <hleb.albau@gmail.com>
2021-04-20 18:58:27 +0300

    rename product

--- update-models.sh
+++ update-models.sh
@@ -1,6 +1,6 @@
-DOCS_DEFINITION_FOLDER="${PWD}/../FaceRecognition-web-openapi"  \
+DOCS_DEFINITION_FOLDER="${PWD}/../FaceSDK-web-openapi"  \
 \
-&& ENUM_MAPPINGS="ImageSource=int,FaceRecognitionResultCode=int,LivenessStatus=int" \
+&& ENUM_MAPPINGS="ImageSource=int,FaceSDKResultCode=int,LivenessStatus=int" \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "$DOCS_DEFINITION_FOLDER:/definitions" \
 openapitools/openapi-generator-cli:v5.0.0-beta2 generate \

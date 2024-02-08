e81e801fe372d5ed4cdc2020f8f7874cca8f66b0
GitHub Action <action@github.com>
2024-02-08 17:18:36 +0000

    update

--- src/Regula.FaceSDK.WebClient/Model/ProcessParamAttributes.cs
+++ src/Regula.FaceSDK.WebClient/Model/ProcessParamAttributes.cs
@@ -1,7 +1,7 @@
 /*
  * Regula FaceSDK Web API
  *
- * Regula FaceSDK Web API # Clients * [JavaScript](https://github.com/regulaforensics/FaceSDK-web-js-client) client for the browser and node.js based on axios * [Java](https://github.com/regulaforensics/FaceSDK-web-java-client) client compatible with jvm and android * [Python](https://github.com/regulaforensics/FaceSDK-web-python-client) 3.5+ client * [C#](https://github.com/regulaforensics/FaceSDK-web-csharp-client) client for .NET & .NET Core 
+ * [Download OpenAPI specification](https://github.com/regulaforensics/FaceSDK-web-openapi) ### Clients * [JavaScript](https://github.com/regulaforensics/FaceSDK-web-js-client) client for the browser and node.js based on axios * [Java](https://github.com/regulaforensics/FaceSDK-web-java-client) client compatible with jvm and android * [Python](https://github.com/regulaforensics/FaceSDK-web-python-client) 3.5+ client * [C#](https://github.com/regulaforensics/FaceSDK-web-csharp-client) client for .NET & .NET Core 
  *
  * The version of the OpenAPI document: 6.1.0
  * 
@@ -34,7 +34,7 @@ namespace Regula.FaceSDK.WebClient.Model
         /// Initializes a new instance of the <see cref="ProcessParamAttributes" /> class.
         /// </summary>
         /// <param name="config">config.</param>
-        public ProcessParamAttributes(List<QualityConfig> config = default(List<QualityConfig>))
+        public ProcessParamAttributes(List<AttributeConfig> config = default(List<AttributeConfig>))
         {
             this.Config = config;
         }
@@ -43,7 +43,7 @@ namespace Regula.FaceSDK.WebClient.Model
         /// Gets or Sets Config
         /// </summary>
         [DataMember(Name="config", EmitDefaultValue=false)]
-        public List<QualityConfig> Config { get; set; }
+        public List<AttributeConfig> Config { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object

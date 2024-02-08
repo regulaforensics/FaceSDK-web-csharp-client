469d9e7bd9c4c3205b5b4e3dcb4b936a6623ef50
Alexander <aliaksandr.hubin@regula.us>
2024-02-08 20:05:19 +0300

    SP-13468: Update openapi-generator-cli version

--- update-models.sh
+++ update-models.sh
@@ -3,4 +3,5 @@ DOCS_DEFINITION_FOLDER="${PWD}/../FaceSDK-web-openapi"  \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "$DOCS_DEFINITION_FOLDER:/definitions" \
 openapitools/openapi-generator-cli:v5.4.0 generate \
 -i /definitions/index.yml -g csharp -o /client/ \
+--global-property models \
 -c /client/csharp-generator-config.json
